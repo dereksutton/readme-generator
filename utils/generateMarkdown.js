da6aa5bd8589c506be91dda53e59290f3edee2c0
Derek Sutton <dereksutton86@gmail.com>
2023-03-28 14:46:51 -0400

    added to generateMarkdown.js and edited index.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,25 +1,29 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+// function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
 const renderLicenseBadge = (license) => {
     if (license !== "none") {
-        return `![license-badge](https://img.shields.io/badge/license/license-${license}-blue.svg)`;
+        return `![${license}](https://img.shields.io/badge/license/license-${license}-blue.svg)`;
     }
     return "";
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function that returns the license link
+// If there is no license, returns an empty string
+const renderLicenseLink = (license) => {
+    if (license !== "none") {
+        return `[${license}](https://opensource.org/licenses/${license})`;
+    }
+    return "";
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function to generate markdown for README
 
-// TODO: Create a function to generate markdown for README
 const generateMarkdown = (data) => {
   return `
     # ${data.title}
+
     ${renderLicenseBadge(data.license)}
+    ${renderLicenseLink(data.license)}
 
     ## Description
     ${data.description}
@@ -37,7 +41,20 @@ const generateMarkdown = (data) => {
     ## Deployed URL
     ${data.url}
 
-    
+    ## Installation
+    ${data.installation}
+
+    ## Test
+    ${data.test}
+
+    ## Usage
+    ${data.usage}
+
+    ## Contributors
+    ${data.contributing}
+
+    ## Contact
+    Built by [github/${data.github}](https://github.com/${data.github}). You can email me [here](mailto:${data.email}).
 `;
 }
 
